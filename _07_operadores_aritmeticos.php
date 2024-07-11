d673bb58668a8268f8c33ff9145f3cefdfecb3b6
kriptonickDevSupport <jorge.armas@kriptonick.com>
2024-07-11 16:00:16 -0600

    7 - Operadores aritmeticos

---
+++ _07_operadores_aritmeticos.php
@@ -0,0 +1,29 @@
+<?php
+
+//Suma
+echo 5 + 5;
+echo "\n";
+
+//Resta
+echo 7 - 5;
+echo "\n";
+
+//Multiplicacion
+echo 5 * 3;
+echo "\n";
+
+//Division
+echo 90 / 3;
+echo "\n";
+
+//Potencia
+echo 5 ** 2;
+echo "\n";
+
+//Modulo
+echo 25 % 3;
+echo "\n";
+
+//Concatenacion de operaciones
+echo (5 ** 2) . " " . (1+6);
+echo "\n";
