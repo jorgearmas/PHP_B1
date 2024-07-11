ceab22e2db4a6cdbb85eeb1aca36d4ae772045c5
kriptonickDevSupport <jorge.armas@kriptonick.com>
2024-07-10 22:22:34 -0600

    2 - Debbuging/Inspeccionar elementos

---
+++ _02_debugging.php
@@ -0,0 +1,15 @@
+<?php
+
+$personas = [
+    "Lucas Films" => 22,
+    "Mr. Beast" => 15,
+    "Donut Media" => 65
+];
+
+//Inspeccionar contenido y detalles de elemento/array
+var_dump($personas);
+echo "\n";
+
+//Inspeccionar solo el contenido de elemento/array
+print_r($personas);
+echo "\n";
