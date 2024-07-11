93fe3a0cef50cec61e1070c5cf5c9537be58d5b9
kriptonickDevSupport <jorge.armas@kriptonick.com>
2024-07-10 22:10:28 -0600

    1 - Imprimir, echo, variables y concatenacion

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+    //Imprimir en pantalla
+    echo "Hola amigos\n";
+
+    //Declaración de variables
+    $nombre = "Mad";
+    $apellido = "Max";
+
+    //Concatenacion con punto "."
+    echo "Mi nombre es: " . $nombre . " " . $apellido . "\n";
+
+    //Concatenacion dentro cadena
+    echo "Hola, mi nombre es: $nombre $apellido \n";
+    /* Al usar comillas dobles php reconoce las variables */
+
+    //Concatenar operaciones matematicas
+    echo "El resultado de 4*5 es: " . (4*5) . "\n";
+?>
