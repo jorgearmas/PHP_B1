7c6db8c78b8eb8bbdf6da6a9a3ea40b6691545a8
kriptonickDevSupport <jorge.armas@kriptonick.com>
2024-07-15 14:11:09 -0600

    9 - Arreglos

---
+++ _09_arreglos.php
@@ -0,0 +1,17 @@
+<?php
+
+//Declaración y asignación
+$edades = [20, 18, 40];
+
+//Accediendo al arreglo
+echo "Edad en posición [0]: $edades[0] \n";
+echo "Edad en posición [1]: $edades[1] \n";
+echo "Edad en posición [2]: $edades[2] \n\n";
+
+//Declaración y asignación con 'array'
+
+$edades = array(20, 18, 40);
+
+echo "Edad en posición [0] con 'array': $edades[0] \n";
+echo "Edad en posición [1] con 'array': $edades[1] \n";
+echo "Edad en posición [2] con 'array': $edades[2] \n";
