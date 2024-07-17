e7a7cf37e0cc900070cdc4bbbfc8f1e6625bf280
jorgearmas <jorgearmasc0496@gmail.com>
2024-07-16 20:17:26 -0600

    13 - Ciclos: Do-While

---
+++ _14_ciclos_do_while.php
@@ -0,0 +1,16 @@
+<?php
+
+// El ciclo do-while se ejecuta al menos una vez sin importar si la condicion se cumple o no
+
+$usernames = [
+    "lucas",
+    "max",
+    "franco"
+];
+
+do {
+    $username = readline("Ingresa usuario: ");
+    echo "\n";
+} while(in_array($username, $usernames));
+
+print_r($usernames);
